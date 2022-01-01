595585308e70997dd62c077e712cca613eea744e
Carlton Gracias <cv.gracias0@gmail.com>
2022-01-01 12:59:42 +0530

    Added a new assignment, initial version

---
+++ Week1/PA1_Sum_of_two_digits.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int digit1, digit2;
+    cin>>digit1;
+    cin>>digit2;
+    cout<<digit1 + digit2;
+}
---
+++ Week1/PA2_Maximum_Pairwise_product.cpp
@@ -0,0 +1,34 @@
+//Input -> n
+//      -> Xo ..Xi... Xn 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+long long getGreatestValueIdx(long long length, vector<long long> input_nums, long long idx_to_ignore = -1){
+    int idx_of_max = 0;
+    for(long long i = 0; i <length; i++){
+        if(i == idx_to_ignore)
+            continue;
+        if(input_nums[i] > input_nums[idx_of_max])
+            idx_of_max = i;
+    }
+    return idx_of_max;
+}
+long long pairWiseProduct(long long length, vector<long long> input_nums){
+    long long idx_of_max = getGreatestValueIdx(length,input_nums);
+    long long idx_of_second_max = getGreatestValueIdx(length,input_nums,idx_of_max);
+    return input_nums[idx_of_max]*input_nums[idx_of_second_max];
+}
+
+
+int main(){
+    long long n;
+    cin>>n;
+    vector <long long> inputs;
+    inputs.resize(n);
+    for (long long i = 0; i<n;i++)
+        cin>>inputs[i];
+    cout<<pairWiseProduct(n,inputs);
+}
+
+
